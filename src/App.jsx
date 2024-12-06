2ffd27060f8e286355d0b26a3d90a2626c2ed199
Ray Hiu Tsun Chan <rchan@cpos.com>
2024-12-05 20:33:05 -0500

    Added font package and desired font family

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from "react";
+
+const App = () => {
+  return <h1 className="text-3xl font-bold underline">d world!</h1>;
+};
+
+export default App;
