c7a26543cc3aa491ad64c5d8a177b8ab11982f0e
Ray Hiu Tsun Chan <rchan@cpos.com>
2024-12-09 14:07:49 -0500

    finished adding Contact and started adding animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { HERO_CONTENT } from "../constants";
 import profilePic from "../assets/kevinRushProfile.png";
+import { motion } from "motion/react";
 
 const Hero = () => {
   return (
@@ -8,12 +9,15 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+            <motion.h1
+              animate={{ x: 100 }}
+              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
+            >
               Ray Chan
-            </h1>
-            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+            </motion.h1>
+            <motion.span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
               Software and Web Developer
-            </span>
+            </motion.span>
             <p className="my-2 max-w-xl py-6 font-light tracking-lighter">
               {HERO_CONTENT}
             </p>
