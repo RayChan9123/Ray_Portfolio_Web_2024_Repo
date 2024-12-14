63d8615c5ff7dbeeabe29a8b7ff3b47b51b0ea61
Ray Hiu Tsun Chan <rchan@cpos.com>
2024-12-13 19:14:18 -0500

    added animation for About and Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,15 @@ import { HERO_CONTENT } from "../constants";
 import profilePic from "../assets/kevinRushProfile.png";
 import { motion } from "motion/react";
 
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5, delay: delay },
+  },
+});
+
 const Hero = () => {
   return (
     <div className="border-b border-neutral-900 pb-4 lg:mb-35">
@@ -10,22 +19,40 @@ const Hero = () => {
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
             <motion.h1
-              animate={{ x: 100 }}
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
               className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
             >
               Ray Chan
             </motion.h1>
-            <motion.span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
+            <motion.span
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
+            >
               Software and Web Developer
             </motion.span>
-            <p className="my-2 max-w-xl py-6 font-light tracking-lighter">
+            <motion.p
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              className="my-2 max-w-xl py-6 font-light tracking-lighter"
+            >
               {HERO_CONTENT}
-            </p>
+            </motion.p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
-            <img src={profilePic} alt="Profile Pic of Ray Chan" />
+            <motion.img
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 0.5 }}
+              src={profilePic}
+              alt="Profile Pic of Ray Chan"
+            />
           </div>
         </div>
       </div>
